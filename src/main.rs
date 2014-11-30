2c3c0761520f3ff7c990424c06abffcf03f68848
jlgoldb2 <jlgoldb2@asu.edu>
2014-11-30 03:22:24 -0700

    simple tcp server is good

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,59 @@
 
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, Jason!");
+    tcp_echoer::listen_then_send_and_receive()
 }
 
+
+
+pub mod tcp_echoer {
+  use std::io::{TcpListener, TcpStream};
+  use std::io::{Acceptor, Listener};
+  use std::time::duration::Duration;
+  use std::io::timer::sleep;
+
+  pub fn listen_then_send_and_receive() {
+
+    println!("TCP-ECHOER CALLED");
+    // bind the listener to the specified address
+    let listener = TcpListener::bind("127.0.0.1:4500");
+
+    // begin listening
+    let mut acceptor = listener.listen();
+
+    spawn( proc() { the_client_sends_a_delayed_message("Did it work?"); });
+
+    for stream in acceptor.incoming() {
+      match stream {
+        Err(e) => { println!("Error occured: {}", e); },
+        Ok(stream) => { spawn(proc() { echo_the_clients_message(stream); }); }
+      }
+    }
+  }
+
+  fn echo_the_clients_message(mut stream: TcpStream) {
+    let message_str = stream
+      .read_to_end()
+      .unwrap();
+
+    let strthing = String::from_utf8(message_str).unwrap();
+    println!("server echos: {}", strthing );
+    println!("press ctrl + C to exit...");
+
+    }
+
+  fn the_client_sends_a_delayed_message(msg: &str) {
+      // delay for a sec then...
+      let delay = Duration::seconds(1);
+      sleep(delay);
+      println!("client sends: {}", msg);
+      // create a mutable unwrapped stream.
+      let mut stream = TcpStream::connect("127.0.0.1:4500").unwrap();
+      //turn the message to bytes
+      let message_bytes = msg.as_bytes();
+      //write to the stream then unwrap the success result
+      stream.write(message_bytes).unwrap();
+  }
+
+}
--- src/tcp-echoer.rs
+++ src/tcp-echoer.rs
@@ -2,30 +2,37 @@
 use std::io::{TcpListener, TcpStream};
 use std::io::{Acceptor, Listener};
 
-fn main() {
-  println!("Hello, world! listener");
-  let listener = TcpListener::bind("127.0.0.1:4500");
+mod tcp_echoer {
+  pub fn listen() {
+    println!("TCP-ECHOER CALLED");
+    // bind the listener to the specified address
+    let listener = TcpListener::bind("127.0.0.1:4500");
 
-  // bind the listener to the specified address
-  let mut acceptor = listener.listen();
+    // begin listening
+    let mut acceptor = listener.listen();
 
-  fn handle_client(mut stream: TcpStream) {
-    let msg_bytes = stream.read_to_end().unwrap();
-    println!("msg_bytes is {}", stream.read_to_end().unwrap());
-
-    //println!("msg_string is {}", msg_bytes.into_ascii() );
-
-
-  // accept connections and process them, spawning a new tasks for each one
-  for stream in acceptor.incoming() {
+    for stream in acceptor.incoming() {
       match stream {
           Err(e) => { println!("Error occured: {}", e); },
           Ok(stream) => spawn(proc() {
               handle_client(stream)
           })
       }
+      drop(acceptor);  // close the socket server
   }
 
-  // close the socket server
-  drop(acceptor);
+  fn handle_client(mut stream: TcpStream) {
+    let message = stream.read_to_end()  // entire stream
+      .unwrap()     // unwrapped the result
+      .into_ascii() // turn the binary values from decimal to ascii
+      .as_slice()   // to string
+    println!("message: {}", message);
+  }
+}
+
+  // accept connections and process them, spawning a new tasks for each one
+
+
+
+
 }
